c1318fb2a29c71646acacb11c99e9d3a1a7fcead
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-03-27 15:28:53 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Header } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header
   ],
   imports: [
     BrowserModule,
