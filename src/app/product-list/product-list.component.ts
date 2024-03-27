10b80e24a74eb6f48225a3b50f09785531f3dacd
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2024-03-27 20:58:01 +0500

    Property-binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,17 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
- product={
-  name:'Iphone 14',
-  price:999,
-  color:'Red',
-  discount:8.5,
-  inStock:1
- }
-   getDiscountedPrice(){
-    return this.product.price - (this.product.price *this.product.discount) / 100;
-   }
+  name:string='John Doe'
+  product = {
+    name: 'Iphone 14',
+    price: 999,
+    color: 'Red',
+    discount: 8.5,
+    inStock: 0,
+    image: '/assets/images/iphone.jpg',
+  };
+  getDiscountedPrice() {
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
+  }
 }
