9998954361612ea416b5c02a767e16d91a04b2b1
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-03-28 07:48:22 +0300

    search|two-way data binding with ngModel

---
+++ src/app/product-list/search/search.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+  searchText:string='Men`s wear'
+
+
+// updateSearchText(event:any){
+// this.searchText=event.target.value
+// }
+
+}
+
