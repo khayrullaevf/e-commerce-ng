1aaf385552e8550092f6f221b39190104959bc30
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-03-28 09:56:36 +0300

    ngIf, discount price , result for

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchText:string='Men`s wear'
+  searchText:string=''
 
 
 // updateSearchText(event:any){
