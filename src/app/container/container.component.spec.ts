3b8dcaf8fedd3ae08f2bda398c0f83d3d7b8ffe0
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-03-28 09:39:37 +0300

    product-list

---
+++ src/app/container/container.component.spec.ts
@@ -0,0 +1,23 @@
+import { ContainerComponent } from './container.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+
+describe('ContainerComponent', () => {
+  let component: ContainerComponent;
+  let fixture: ComponentFixture<ContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ContainerComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
